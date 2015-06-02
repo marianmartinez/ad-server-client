7cb44b26f13d7cf0bff8ef83796c14a659933655
Juan Antona <juan.antonadelval@gmail.com>
2015-06-02 15:19:36 +0200

    ajax request with parameters

--- js-client/adserver.js
+++ js-client/adserver.js
@@ -1,16 +1,20 @@
-$( document ).ready(function() {
+$( document ).ready(function() 
+{
     
     var url1 = "./response.json";
     var url2 = "./response1.json";
 
 	$.ajax({
 	  method: "GET",
-	  url: "/advertisements"
+	  url: "/advertisements",
+	  dataType: "json",
+	  data: {some_parameter: 'hello'}
 	})
-	.done(function( data ) {
-      var imgSource = data.newAdvertisement;
+	.done(function( response ) {
+      var imgSource = response.newAdvertisement;
       $('#banner').attr("src", imgSource);
  	});
+
 });
 
  
